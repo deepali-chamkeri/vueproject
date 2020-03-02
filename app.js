6fcc7f8874e7e6b4d758c6d8ab2ef488a1293a59
deepali-chamkeri <60977269+deepali-chamkeri@users.noreply.github.com>
2020-03-02 19:01:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 Vue.component('movie-list',{
-  // props:{ imId: 'tt1201607'},
   template:`
   <div class="movie-list">
   <div class="row">
   <div class="col-md-4" v-for ="sts in status">
-   <img :src="sts.Poster">
+  <a v-bind:href="'movie.html?p=' + sts.imdbID"><img :src="sts.Poster"></a>
   <h5>{{ sts.Title }}</h5>
-  <a v-bind:href="'movie.html?p=' + sts.imdbID"> Movie Details </a>
-<!--    <br/>-->
-<!--    <a v-on:click="doSomething(sts.imdbID)"> Movie ... </a>-->
+  <a v-bind:href="'movie.html?p=' + sts.imdbID" class="btn btn-primary"> View Summary </a>
 </div>
 </div>
 </div>
@@ -35,29 +32,31 @@ return {
       });
      
     }
-    // ,
-    //   doSomething : function(imd) {
-    //     alert ("Here " + imd);
-    //   }
   }
 })
 Vue.component('movie-desc',
     {
-      // props:{ imId: 'tt1201607'},
   template:`
   <div class="movie-desc">
-      <div class="row">
-          <div class="col-md-6">
+      <div class="row"> 
+          <div class="col-md-4">
           <img :src="status1.Poster">
-          <h6>{{ status1.Title }}</h6>
-            <ul>
-<!--                <li>ID : {{imId}}</li>-->
-              <li>imdbID:{{status1.imdbID}}</li>
-              <li>Year:{{status1.Year}}</li>
-              <li>Type:{{status1.Type}}</li>
-              <li>Director:{{status1.Director}}</li>
-              <li>Actors:{{status1.Actors}}</li>
-            </ul>         
+          <h5>{{ status1.Title }}</h5>
+          </div>
+          <div class="col-md-8">
+          <ul class="list-group">
+          <li class="list-group-item"><strong>Genre:</strong>{{ status1.Genre }}</li>
+          <li class="list-group-item"><strong>Released:</strong>{{ status1.Released }}</li>
+          <li class="list-group-item"><strong>Rated:</strong>{{ status1.Rated }}</li>
+          <li class="list-group-item"><strong>Language:</strong>{{ status1.Language }}</li>
+          <li class="list-group-item"><strong>Director:</strong>{{ status1.Director }}</li>
+          <li class="list-group-item"><strong>Writer:</strong>{{ status1.Writer }}</li>
+          <li class="list-group-item"><strong>Actors:</strong>{{ status1.Actors }}</li>
+          <li class="list-group-item"><strong>Awards:</strong>{{ status1.Awards }}</li>
+          <li class="list-group-item"><strong>Country:</strong>{{ status1.Country }}</li>
+          <li class="list-group-item"><strong>Type:</strong>{{ status1.Type }}</li>
+          <li class="list-group-item"><strong>DVD:</strong>{{ status1.DVD }}</li>
+          </ul>       
           </div>
        </div>
   </div>
