75fb87da1fd7188ee1a681e554c2ae1a337b4dad
deepali-chamkeri <60977269+deepali-chamkeri@users.noreply.github.com>
2020-03-19 23:08:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ return {
 
       this.status1='loading';
       var vm=this;
-      axios.get('https://www.omdbapi.com/?apikey=bfce84f9&i=tt3896198')
+      axios.get('https://www.omdbapi.com/?apikey=bfce84f9&i=tt1201607')
       .then(function(response){
         console.log(response)
         vm.status1 = response.data;
